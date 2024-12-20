d3f23def7131fe2a8efb95dd92675f0f0dffe9d4
KingMalik23 <malikdoumbia23@gmail.com>
2024-12-20 17:32:19 -0500

    styling edits

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import {formatISO9075} from "date-fns";
+
 
 export default function PostPage() {
     const [postInfo,setPostInfo] = useState(null);
@@ -13,15 +15,18 @@ export default function PostPage() {
                 });
             });
     }, []);
+
     if(!postInfo) return '';
 
     return(
         <div className="post-page">
+            <h1>{postInfo.title}</h1>
+            <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+            <div className="author">by @{postInfo.author.username}</div>
             <div className="image">
             <img src={'http://localhost:3001/${postInfo.cover}'} alt=""/>
             </div>
-            <h1>{postInfo.title}</h1>
-            <div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
+            <div classNam="content" dangerouslySetInnerHTML={{__html:postInfo.content}}/>
         </div>
     );
 }
